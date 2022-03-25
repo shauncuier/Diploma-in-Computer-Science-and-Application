0fd7fbd56d60e690e7473439670956168983dbaa
Shaun <shauncuier@gmail.com>
2022-03-26 02:10:27 +0600

    Problem 1 2 Solved

---
+++ c lang/Problem 1/problem1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a , b, diff;
+    printf("Enter two number:");
+    scanf("%d%d",&a,&b);
+    diff = a - b;
+    printf("The difference is %d",diff);
+    return 0;
+
+}
---
+++ c lang/Problem 1/problem2.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main()
+{
+    float C , F;
+    printf("Enter Centigrade:");
+    scanf("%f",&C);
+    F = ((C/5)*9)+32;
+    printf("Fahrenheit is %f",F);
+    return 0;
+
+}
