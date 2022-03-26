f95296f88438a096f9f332ccc27b0bf6d333d211
Shaun <shauncuier@gmail.com>
2022-03-26 11:08:31 +0600

    Problem 3 Error

---
+++ c lang/Problem 3/problem3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter three Numbers:");
+    scanf("%d%d%d", &a,&b,&c);
+    if (a<b)
+        if(a<c)
+        printf("Smallest = %d,a");
+        else
+        printf("Smallest = %d,c");
+    else
+        if(b<c)
+        printf("Smallest = %d,b");
+        else
+        printf("Smallest = %d,c");
+
+    return 0;
+
+
+}
