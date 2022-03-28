37d57f8823d65879b3f849dcf353f28a6cc3aa2e
Shaun <shauncuier@gmail.com>
2022-03-29 04:01:42 +0600

    while loop

--- c lang/Practice/hello.c
+++ c lang/Practice/hello.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int num = 50;
-    if(num >= 1 || num <= 10) {
-        printf("yes\n");
-    }
-    else {
-        printf("no\n");
+    int n = 17;
+    int i = 1;
+    while (i <= 10) {
+        printf("%d X %d = %d\n", n, i, n*i);
+        i = i + 1;
     }
     return 0;
-
 
 
 }
