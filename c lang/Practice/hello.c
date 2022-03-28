9a1d8b86b612e0b545330e17b909cc8c72a07c86
Shaun <shauncuier@gmail.com>
2022-03-29 03:12:17 +0600

    getchar()

--- c lang/Practice/hello.c
+++ c lang/Practice/hello.c
@@ -3,8 +3,7 @@ int main()
 {
     char ch;
     printf("Enter the first letter of your name: ");
-    scanf("%c", &ch);
+    ch = getchar();
     printf("The first letter of your name is: %c\n", ch);
-
     return 0;
 }
