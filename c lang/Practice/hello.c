f523fa7ed254e23a0dc5072f1e52e4443610d7e7
Shaun <shauncuier@gmail.com>
2022-03-29 02:58:16 +0600

    0.4lf

--- c lang/Practice/hello.c
+++ c lang/Practice/hello.c
@@ -1,21 +1,11 @@
-
 #include <stdio.h>
 int main()
 {
-     int a;
-    a = 1000;
-    printf("Value of a is %d\n", a);
-    a = 21000;
-    printf("Value of a is %d\n", a);
-    a = 10000000;
-    printf("Value of a is %d\n", a);
-    a = 10000000;
-    printf("Value of a is %d\n", a);
-    a = 100020004000503;
-    printf("Value of a is %d\n", a);
-    a = 4325987632;
-    printf("Value of a is %d\n", a);
+    double a, b, sum;
+    a = 9.5;
+    b = 8.743;
+    sum = a + b;
+    printf("Sum is: %lf\n", sum);
+    printf("Sum is: %0.4lf\n", sum);
     return 0;
-
 }
-
