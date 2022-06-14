0858993ccbf6bd76aff4ad7e7f5603c9ed08a7b4
Md. Nahidul Islam <nahidulss@gmail.com>
2022-06-15 01:18:00 +0600

    7 solved

--- c lang/TMA – I/7/7.c
+++ c lang/TMA – I/7/7.c
@@ -1,22 +1,21 @@
 //7.	Evaluate the polynomial: Where the value of x is given from the keyboard.
 #include <stdio.h>
-#include <stdlib.h>
-#define MAXSIZE 10
+#include<math.h>
 
 void main()
 {
-  int a[MAXSIZE];
+ 
   int i, N;
-  float x, polySum=0;
+  float x, polySum=0,common_term;
   printf("Enter how many terms in the polynomial:"); 
   scanf("%d", & N);
   printf("Enter the value of x:"); 
   scanf("%f", & x);
   common_term=(x-1)/x;
-  
+
   for (i = 1; i <= N; i++) {
     polySum += pow(common_term,i)/i;  /* calculating the polysum  */
   }
 
-  printf("\nSum of the polynomial = %6.2f\n", polySum); /* displays the sum */
+  printf("\nSum of the polynomial = %f\n", polySum); /* displays the sum */
 }
