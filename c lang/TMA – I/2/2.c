bc0822a16cf1387e6e4bfcef61beef0f37c528e2
Md. Nahidul Islam <nahidulss@gmail.com>
2022-06-15 00:16:20 +0600

    2 updated

--- c lang/TMA – I/2/2.c
+++ c lang/TMA – I/2/2.c
@@ -1,10 +1,11 @@
 // 2.	Write a C program to find area and circumference of a circle by defining the value of PI.
 #include <stdio.h>
+#define PI 3.14
 int main() {
 
 int rad;
 
-float PI=3.14, area, ci;
+float area, ci;
 
 printf("\nEnter radius of circle: ");
 
