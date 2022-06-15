856b1cbb8dc5da8bdf15bb19c8a4bbec70fa1d06
Md. Nahidul Islam <nahidulss@gmail.com>
2022-06-15 11:40:33 +0600

    10 solved

--- c lang/TMA – I/10/10.c
+++ c lang/TMA – I/10/10.c
@@ -1,2 +1,15 @@
 //10. Write a C program to find simple interest. [Simple interest formula: A = P(1 + rt) where P is the Principal amount of money to be invested at an Interest Rate R% per year for t Time (years) where r is in decimal form; r=R/100]
+#include <stdio.h>
+
+ int main() { 
+    float principle, time, rate, SI; 
+    /* Input principle, rate and time */ printf("Enter principle (amount): "); scanf("%f", &principle); 
+    printf("Enter time: "); 
+    scanf("%f", &time); printf("Enter rate: "); scanf("%f", &rate); 
+    /* Calculate simple interest */
+     SI = principle * (1+time *(rate/100));
+     /* Print the resultant value of SI */ printf("Simple Interest = %f", SI);
+     return 0; 
+   }
+
 
