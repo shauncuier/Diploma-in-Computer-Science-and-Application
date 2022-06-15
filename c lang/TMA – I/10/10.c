6d28cc58601323638725b73f3754054e874e363f
Md. Nahidul Islam <nahidulss@gmail.com>
2022-06-15 11:41:28 +0600

    10 solved with indent

--- c lang/TMA – I/10/10.c
+++ c lang/TMA – I/10/10.c
@@ -3,12 +3,15 @@
 
  int main() { 
     float principle, time, rate, SI; 
-    /* Input principle, rate and time */ printf("Enter principle (amount): "); scanf("%f", &principle); 
+    /* Input principle, rate and time */ 
+    printf("Enter principle (amount): "); scanf("%f", &principle); 
     printf("Enter time: "); 
-    scanf("%f", &time); printf("Enter rate: "); scanf("%f", &rate); 
+    scanf("%f", &time); printf("Enter rate: "); 
+    scanf("%f", &rate); 
     /* Calculate simple interest */
      SI = principle * (1+time *(rate/100));
-     /* Print the resultant value of SI */ printf("Simple Interest = %f", SI);
+     /* Print the resultant value of SI */
+      printf("Simple Interest = %f", SI);
      return 0; 
    }
 
