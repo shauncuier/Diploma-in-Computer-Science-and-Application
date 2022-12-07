8dad22258b47c5fa98023ffc353c6bbc487c912d
Jashedul Islam Shaun <71716179+shauncuier@users.noreply.github.com>
2022-12-08 00:59:22 +0600

    VB Arihmetic Oparetor

--- Visual Basic/FreeCodeCamp/Form1.Designer.vb
+++ Visual Basic/FreeCodeCamp/Form1.Designer.vb
@@ -22,30 +22,16 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.btnCal = New System.Windows.Forms.Button()
         Me.SuspendLayout()
-        '
-        'btnCal
-        '
-        Me.btnCal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnCal.Location = New System.Drawing.Point(303, 85)
-        Me.btnCal.Name = "btnCal"
-        Me.btnCal.Size = New System.Drawing.Size(133, 47)
-        Me.btnCal.TabIndex = 0
-        Me.btnCal.Text = "Calculate"
-        Me.btnCal.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.btnCal)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents btnCal As Button
 End Class
--- Visual Basic/FreeCodeCamp/Form1.vb
+++ Visual Basic/FreeCodeCamp/Form1.vb
@@ -1,28 +1,5 @@
 ﻿Public Class Form1
-    Private Sub btnCal_Click(sender As Object, e As EventArgs) Handles btnCal.Click
-
-        Dim decPrice As Decimal
-        Dim iQuantity As Integer
-        Dim decDiscount As Decimal
-
-        Dim decTotatCost As Decimal
-
-
-        decPrice = 5
-        iQuantity = 10
-        decDiscount = 2
 
 
 
-        decTotatCost = (decPrice - decDiscount) * iQuantity
-
-        ' BO(DM)(AS) - Brackets Order Div Mult Add Sub
-
-        MsgBox(decTotatCost)
-
-
-
-
-
-    End Sub
 End Class
