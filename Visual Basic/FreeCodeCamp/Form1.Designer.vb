4dbddf6a2f53f85ce3632662c01479baa3e5c788
Shaun <shauncuier@gmail.com>
2023-06-12 00:42:32 +0600

    Partial Commit

--- Visual Basic/FreeCodeCamp/Form1.Designer.vb
+++ Visual Basic/FreeCodeCamp/Form1.Designer.vb
@@ -22,16 +22,52 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.lblCountry = New System.Windows.Forms.Label()
+        Me.txtCountry = New System.Windows.Forms.TextBox()
+        Me.btnGreet = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'lblCountry
+        '
+        Me.lblCountry.AutoSize = True
+        Me.lblCountry.Location = New System.Drawing.Point(41, 88)
+        Me.lblCountry.Name = "lblCountry"
+        Me.lblCountry.Size = New System.Drawing.Size(170, 16)
+        Me.lblCountry.TabIndex = 0
+        Me.lblCountry.Text = "What Country are you from?"
+        '
+        'txtCountry
+        '
+        Me.txtCountry.Location = New System.Drawing.Point(261, 87)
+        Me.txtCountry.Name = "txtCountry"
+        Me.txtCountry.Size = New System.Drawing.Size(359, 22)
+        Me.txtCountry.TabIndex = 1
+        '
+        'btnGreet
+        '
+        Me.btnGreet.Location = New System.Drawing.Point(294, 157)
+        Me.btnGreet.Name = "btnGreet"
+        Me.btnGreet.Size = New System.Drawing.Size(217, 97)
+        Me.btnGreet.TabIndex = 2
+        Me.btnGreet.Text = "Greeting"
+        Me.btnGreet.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnGreet)
+        Me.Controls.Add(Me.txtCountry)
+        Me.Controls.Add(Me.lblCountry)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents lblCountry As Label
+    Friend WithEvents txtCountry As TextBox
+    Friend WithEvents btnGreet As Button
 End Class
--- Visual Basic/FreeCodeCamp/Form1.vb
+++ Visual Basic/FreeCodeCamp/Form1.vb
@@ -1,5 +1,23 @@
 ﻿Public Class Form1
+    Private Sub btnGreet_Click(sender As Object, e As EventArgs) Handles btnGreet.Click
+
+        Dim stCountry As String
+        stCountry = txtCountry.Text
+        stCountry = stCountry.ToUpper
 
 
 
+        If stCountry = "BANGLADESH" Then
+            MsgBox("Sovo Sokal")
+        ElseIf stCountry = "INDIA" Then
+            MsgBox("Soba mubarak")
+
+        Else
+            MsgBox("Hello")
+
+        End If
+
+
+
+    End Sub
 End Class
